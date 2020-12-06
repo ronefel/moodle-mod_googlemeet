f88ef9ff73be37259ebe3917228bd33f08e06a3f
ronefel <ronefel@hotmail.com>
2020-12-06 01:45:29 -0400

    Implementing metadata provider for the googlemeet_notify_done table

--- lang/en/googlemeet.php
+++ lang/en/googlemeet.php
@@ -25,20 +25,13 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['recurrenceeventdate'] = 'Recurrence of the event date';
-$string['recurrenceeventdate_help'] = 'This function makes it possible to create multiple recurrences from the event date.
-<br>* <strong>Repeat on</strong>: Select the days of the week that your class will meet (for example, Monday / Wednesday / Friday).
-<br>* <strong>Repeat every</strong>: This allows for a frequency setting. If your class will meet every week, select 1; will meet every two weeks, select 2; every 3 weeks, select 3, etc.
-<br>* <strong>Repeat until</strong>: Select the last day of the meeting (the last day you want to take the recurring date of the event).
-';
 $string['apikey'] = 'API key';
+$string['checkweekdays'] = 'Select the days of the week that fall within the selected date range.';
 $string['clientid'] = 'OAuth client ID';
 $string['clientid_desc'] = '<a href="https://github.com/ronefel/moodle-mod_googlemeet/wiki/How-to-create-the-API-key-and-OAuth-client-ID" target="_blank">How to create the API key and OAuth client ID</a>';
-$string['checkweekdays'] = 'Select the days of the week that fall within the selected date range.';
 $string['date'] = 'Date';
 $string['duration'] = 'Duration';
-$string['notify'] = 'Send notification to the student';
-$string['notify_help'] = 'If checked, a notification will be sent to the student about the start date of the event.';
+$string['earlierto'] = 'The event date cannot be earlier than the course start date ({$a}).';
 $string['emailcontent'] = 'Email content';
 $string['emailcontent_default'] = '<p>Hi %userfirstname%,</p>
 <p>This reminder is to remind you that there will be a Google meet event in %coursename%</p>
@@ -61,11 +54,11 @@ $string['entertheroom'] = 'Enter the room';
 $string['eventdate'] = 'Event date';
 $string['from'] = 'from';
 $string['generateurlroom'] = 'Generate Room URL';
-$string['googlemeet:addinstance'] = 'Add a new google meet™ for Moodle';
+$string['googlemeet:addinstance'] = 'Add a new Google Meet™ for Moodle';
 $string['googlemeet:editrecording'] = 'Edit recordings';
 $string['googlemeet:removerecording'] = 'Remove recordings';
-$string['googlemeet:syncgoogledrive'] = 'Sync with google drive';
-$string['googlemeet:view'] = 'View google meet™ for Moodle content';
+$string['googlemeet:syncgoogledrive'] = 'Sync with Google Drive';
+$string['googlemeet:view'] = 'View Google Meet™ for Moodle content';
 $string['hide'] = 'Hide';
 $string['invalideventenddate'] = 'This date can not be earlier than the "Event date"';
 $string['invalideventendtime'] = 'The end time must be greater than start time';
@@ -74,15 +67,15 @@ $string['jstableinfo'] = 'Showing {start} to {end} of {rows} recordings';
 $string['jstableinfofiltered'] = 'Showing {start} to {end} of {rows} recordings (filtered from {rowsTotal} recordings)';
 $string['jstableloading'] = 'Loading...';
 $string['jstablenorows'] = 'No recording found';
-$string['jstablesearch'] = 'Search...';
 $string['jstableperpage'] = '{select} recordings per page';
+$string['jstablesearch'] = 'Search...';
 $string['lastsync'] = 'Last sync:';
 $string['loading'] = 'Loading';
 $string['messageprovider:notification'] = 'Google Meet event start reminder';
 $string['minutesbefore'] = 'Minutes before';
 $string['minutesbefore_help'] = 'Number of minutes before the start of the event when the notification should be send.';
 $string['modulename'] = 'Google Meet™ for Moodle';
-$string['modulename_help'] = 'The Google Meet™ for Moodle module allows a teacher to create a meeting room as a course resource.
+$string['modulename_help'] = 'The Google Meet™ module for Moodle allows the teacher to create a Google Meet room as a course resource and after the meetings make available to the students the recordings, saved in Google Drive.
 <p>©2018 Google LLC All rights reserved.<br/>
 Google Meet and the Google Meet logo are registered trademarks of Google LLC.</p>
 ';
@@ -95,6 +88,8 @@ $string['notfoundrecordingname'] = 'No recording found with the name';
 $string['notfoundrecordingsfolder'] = 'The "Meet Recordings" folder was not found in Google Drive.';
 $string['notification'] = 'Notification';
 $string['notificationexpanded'] = 'Notification expanded';
+$string['notify'] = 'Send notification to the student';
+$string['notify_help'] = 'If checked, a notification will be sent to the student about the start date of the event.';
 $string['notifycationexpanded_desc'] = 'Show the "Notification" settings as expanded by default when creating new Room.';
 $string['notifytask'] = 'Google Meet™ for Moodle notification task';
 $string['notpossiblesync'] = 'Is not possible to synchronize with an account other than the one that created the room.';
@@ -102,11 +97,20 @@ $string['or'] = 'or';
 $string['play'] = 'Play';
 $string['pluginadministration'] = 'Google Meet™ for Moodle administration';
 $string['pluginname'] = 'Google Meet™ for Moodle';
-$string['privacy:metadata'] = 'The Google Meet™ for Moodle resource plugin does not store any personal data.';
+$string['privacy:metadata:googlemeet_notify_done'] = 'Records notifications sent to users about the start of events. This data is temporary and is deleted after the event start date.';
+$string['privacy:metadata:googlemeet_notify_done:eventid'] = 'The event ID';
+$string['privacy:metadata:googlemeet_notify_done:userid'] = 'The user ID';
+$string['privacy:metadata:googlemeet_notify_done:timesent'] = 'The timestamp indicating when the user received a notification';
 $string['earlierto'] = 'The event date cannot be earlier than the course start date ({$a}).';
 $string['recording'] = 'Recording';
 $string['recordings'] = 'Recordings';
 $string['recordingswiththename'] = 'Recordings with the name:';
+$string['recurrenceeventdate'] = 'Recurrence of the event date';
+$string['recurrenceeventdate_help'] = 'This function makes it possible to create multiple recurrences from the event date.
+<br>* <strong>Repeat on</strong>: Select the days of the week that your class will meet (for example, Monday / Wednesday / Friday).
+<br>* <strong>Repeat every</strong>: This allows for a frequency setting. If your class will meet every week, select 1; will meet every two weeks, select 2; every 3 weeks, select 3, etc.
+<br>* <strong>Repeat until</strong>: Select the last day of the meeting (the last day you want to take the recurring date of the event).
+';
 $string['repeatasfollows'] = 'Repeat the event date above as follows';
 $string['repeatevery'] = 'Repeat every';
 $string['repeaton'] = 'Repeat on';
--- lang/pt_br/googlemeet.php
+++ lang/pt_br/googlemeet.php
@@ -25,20 +25,13 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['recurrenceeventdate'] = 'Recorrência da data do evento';
-$string['recurrenceeventdate_help'] = 'Esta função possibilita a criação de várias recorrências da data do evento.
-<br>* <strong>Repetir</strong>: Selecione os dias da semana em que sua classe se reunirá (por exemplo, segunda-feira / quarta-feira / sexta-feira).
-<br>* <strong>Repetir a cada</strong>: Isso permite uma configuração de frequência. Se sua classe se reunirá todas as semanas, selecione 1; se reunirá a cada duas semanas, selecione 2; a cada 3 semanas, selecione 3, e assim por diante.
-<br>* <strong>Repetir até</strong>: Selecione o último dia de reunião (o último dia que você deseja levar a recorrência da data do evento).
-';
 $string['apikey'] = 'Chave de API';
+$string['checkweekdays'] = 'Selecione os dias da semana que se enquadram no intervalo de datas selecionado.';
 $string['clientid'] = 'ID do cliente OAuth';
 $string['clientid_desc'] = '<a href="https://github.com/ronefel/moodle-mod_googlemeet/wiki/Como-criar-a-chave-de-API-e-o-ID-do-cliente-OAuth" target="_blank">Como criar a chave de API e o ID do cliente OAuth</a>';
-$string['checkweekdays'] = 'Selecione os dias da semana que se enquadram no intervalo de datas selecionado.';
 $string['date'] = 'Data';
 $string['duration'] = 'Duração';
-$string['notify'] = 'Enviar notificação para o estudante';
-$string['notify_help'] = 'Se marcada, uma notificação será enviada ao aluno sobre a data de início do evento.';
+$string['earlierto'] = 'A data do evento não pode ser anterior à data de início do curso ({$a}).';
 $string['emailcontent'] = 'Conteúdo do e-mail';
 $string['emailcontent_default'] = '<p>Olá %userfirstname%,</p>
 <p>Este lembrete é para lembrar você de que haverá um evento do Google Meet em %coursename%</p>
@@ -61,11 +54,11 @@ $string['entertheroom'] = 'Entrar na sala';
 $string['eventdate'] = 'Data do evento';
 $string['from'] = 'das';
 $string['generateurlroom'] = 'Gerar URL da sala';
-$string['googlemeet:addinstance'] = 'Adicionar novo google meet™ para Moodle';
+$string['googlemeet:addinstance'] = 'Adicionar novo Google Meet™ para Moodle';
 $string['googlemeet:editrecording'] = 'Editar as gravações';
 $string['googlemeet:removerecording'] = 'Remover as gravações';
-$string['googlemeet:syncgoogledrive'] = 'Sincronizar com o google drive';
-$string['googlemeet:view'] = 'Ver google meet™ para Moodle';
+$string['googlemeet:syncgoogledrive'] = 'Sincronizar com o Google Drive';
+$string['googlemeet:view'] = 'Ver Google Meet™ para Moodle';
 $string['hide'] = 'Ocultar';
 $string['invalideventenddate'] = 'Esta data não pode ser anterior à "Data do evento"';
 $string['invalideventendtime'] = 'O horário de término deve ser maior que o horário de início';
@@ -74,15 +67,15 @@ $string['jstableinfo'] = 'Mostrando {start} a {end} de {rows} gravações';
 $string['jstableinfofiltered'] = 'Mostrando {start} a {end} de {rows} gravações (filtrado de {rowsTotal} gravações)';
 $string['jstableloading'] = 'Carregando...';
 $string['jstablenorows'] = 'Nenhuma gravação encontrada';
-$string['jstablesearch'] = 'Procurar...';
 $string['jstableperpage'] = '{select} gravações por página';
+$string['jstablesearch'] = 'Procurar...';
 $string['lastsync'] = 'Última sincronização:';
 $string['loading'] = 'Carregando';
 $string['messageprovider:notification'] = 'Lembrete de início do evento do Google Meet';
 $string['minutesbefore'] = 'Minutos antes';
 $string['minutesbefore_help'] = 'Número de minutos antes do início do evento quando a notificação deve ser enviada.';
 $string['modulename'] = 'Google Meet™ para Moodle';
-$string['modulename_help'] = 'O módulo Google Meet™ para Moodle permite que o professor crie uma sala do Google Meet como recurso do curso.
+$string['modulename_help'] = 'O módulo Google Meet™ para Moodle permite ao professor criar uma sala do Google Meet como recurso do curso e, após as reuniões, disponibilizar as gravações aos alunos, salvas no Google Drive.
 <p>©2018 Google LLC All rights reserved.<br/>
 Google Meet and the Google Meet logo are registered trademarks of Google LLC.</p>
 ';
@@ -95,6 +88,8 @@ $string['notfoundrecordingname'] = 'Nenhuma gravação encontrada com o nome';
 $string['notfoundrecordingsfolder'] = 'A pasta "Meet Recordings" não foi encontrada no Google Drive.';
 $string['notification'] = 'Notificação';
 $string['notificationexpanded'] = 'Notificação expandida';
+$string['notify'] = 'Enviar notificação para o estudante';
+$string['notify_help'] = 'Se marcada, uma notificação será enviada ao aluno sobre a data de início do evento.';
 $string['notifycationexpanded_desc'] = 'Mostrar as configurações de "Notificação" expandidas por padrão ao criar uma nova sala.';
 $string['notifytask'] = 'Tarefa de notificação do Google Meet™ para Moodle';
 $string['notpossiblesync'] = 'Não é possível sincronizar com uma conta diferente daquela que criou a sala.';
@@ -102,11 +97,20 @@ $string['or'] = 'ou';
 $string['play'] = 'Reproduzir';
 $string['pluginadministration'] = 'Administração do Google Meet™ para Moodle';
 $string['pluginname'] = 'Google Meet™ para Moodle';
-$string['privacy:metadata'] = 'O plugin de recurso Google Meet™ para Moodle não armazena nenhum dado pessoal.';
+$string['privacy:metadata:googlemeet_notify_done'] = 'Registra notificações enviadas aos usuários sobre o início dos eventos. Esses dados são temporários e são excluídos após a data de início do evento.';
+$string['privacy:metadata:googlemeet_notify_done:eventid'] = 'O ID do evento';
+$string['privacy:metadata:googlemeet_notify_done:userid'] = 'O ID do usuário';
+$string['privacy:metadata:googlemeet_notify_done:timesent'] = 'O timestamp indicando quando o usuário recebeu uma notificação';
 $string['earlierto'] = 'A data do evento não pode ser anterior à data de início do curso ({$a}).';
 $string['recording'] = 'Gravação';
 $string['recordings'] = 'Gravações';
 $string['recordingswiththename'] = 'Gravações com o nome:';
+$string['recurrenceeventdate'] = 'Recorrência da data do evento';
+$string['recurrenceeventdate_help'] = 'Esta função possibilita a criação de várias recorrências da data do evento.
+<br>* <strong>Repetir</strong>: Selecione os dias da semana em que sua classe se reunirá (por exemplo, segunda-feira / quarta-feira / sexta-feira).
+<br>* <strong>Repetir a cada</strong>: Isso permite uma configuração de frequência. Se sua classe se reunirá todas as semanas, selecione 1; se reunirá a cada duas semanas, selecione 2; a cada 3 semanas, selecione 3, e assim por diante.
+<br>* <strong>Repetir até</strong>: Selecione o último dia de reunião (o último dia que você deseja levar a recorrência da data do evento).
+';
 $string['repeatasfollows'] = 'Repita a data do evento acima da seguinte forma';
 $string['repeatevery'] = 'Repetir a cada';
 $string['repeaton'] = 'Repetir';
